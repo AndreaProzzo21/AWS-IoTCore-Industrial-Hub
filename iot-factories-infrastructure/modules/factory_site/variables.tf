530cfe68a528bc1656d10d4ddc4fab7cecd7aa31
AndreaProzzo21 <and.prozzo@gmail.com>
2026-03-03 22:40:54 +0100

    Added README

--- iot-factories-infrastructure/modules/factory_site/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "factory_name" {
-  type        = string
-  description = "Nome identificativo della fabbrica"
-}
-
-variable "device_list" {
-  type        = list(string)
-  description = "Lista dei nomi dei macchinari/device in questa fabbrica"
-}
